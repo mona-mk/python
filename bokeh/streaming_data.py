7cea2a66c1fe028361bff7a5eb1a2fca9bfc6ab6
Mona Kanafi <mona@roadcloud.com>
2023-01-26 15:13:40 +0200

    Add select widget for phase lag

--- bokeh/streaming_data.py
+++ bokeh/streaming_data.py
@@ -1,7 +1,8 @@
 from bokeh.plotting import figure
 from bokeh.io import curdoc
-from bokeh.models import ColumnDataSource
-from math import pi, cos, sin
+from bokeh.models import ColumnDataSource, Select
+from bokeh.layouts import layout
+from math import pi, cos, sin, radians
 import numpy as np
 
 
@@ -11,8 +12,8 @@ for i, r in enumerate(range(1, 4)):
     data[f"x{i}"] = [r * cos(t) for t in np.linspace(0, 2 * pi, num=50)]
     data[f"y{i}"] = [r * sin(t) for t in np.linspace(0, 2 * pi, num=50)]
 source0 = ColumnDataSource(data)
-source1 = ColumnDataSource(data={"x": [], "y": []})
-source2 = ColumnDataSource(data={"x": [], "y": []})
+source1 = ColumnDataSource(data={"x": [], "y": [], "theta": []})
+source2 = ColumnDataSource(data={"x": [], "y": [], "theta": []})
 
 # plot circles & customise
 f = figure(x_range=(-4, 4), y_range=(-4, 4))
@@ -48,7 +49,7 @@ f.scatter(
 def point_on_circle(theta, r):
     x = [r * cos(theta)]
     y = [r * sin(theta)]
-    return {"x": x, "y": y}
+    return {"x": x, "y": y, "theta": [theta]}
 
 
 theta = 0
@@ -57,9 +58,24 @@ theta = 0
 def update():
     global theta
     theta += 0.1
-    source1.stream(point_on_circle(theta * 4, 2), rollover=1)  # add some lag
+    source1.stream(point_on_circle(theta, 2), rollover=1)  # add some lag
     source2.stream(point_on_circle(theta, 3), rollover=1)
 
 
-curdoc().add_root(f)
+# Add user input phase lag
+select = Select(
+    title="Phase lag:", value="0", options=["0", "90", "180", "270"]
+)
+
+
+def select_update(attr, old, new):
+    global theta
+    # get current angle and add the phase lag
+    phase_lag = radians(float(select.value))
+    theta = float(source1.data["theta"][0]) + phase_lag
+
+
+select.on_change("value", select_update)
+
+curdoc().add_root(layout([[f], [select]]))
 curdoc().add_periodic_callback(update, 1000)
