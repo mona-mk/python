3e9eedb37c8b61e5aca2a5a1fa8f7dcf3fa3e28a
Mona Kanafi <mona@roadcloud.com>
2023-01-26 15:14:07 +0200

    Add bokeh plot with tooltip

---
+++ bokeh/airports.py
@@ -0,0 +1,39 @@
+import xyzservices.providers as xyz
+from bokeh.plotting import figure
+from bokeh.io import output_file, show
+from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.sampledata.airport_routes import airports
+from pyproj import Transformer
+
+# prepare data
+transformer = Transformer.from_crs("EPSG:4326", "EPSG:3857")
+airports[["x", "y"]] = airports.apply(
+    lambda row: transformer.transform(row.Latitude, row.Longitude),
+    axis=1,
+    result_type="expand",
+)
+airports_data = ColumnDataSource(airports)
+
+# instantiate figure object and add objects
+f = figure(x_axis_type="mercator", y_axis_type="mercator")
+f.circle(
+    x="x",
+    y="y",
+    size=10,
+    fill_color="#F46B42",
+    line_color="white",
+    line_width=0.5,
+    source=airports_data,
+)
+f.add_tile(xyz.OpenStreetMap.Mapnik)
+
+# add tooltip
+hover = HoverTool(
+    tooltips=[("Name", "@Name"), ("City", "@City"), ("Country", "@Country")]
+)
+f.add_tools(hover)
+
+# output file
+output_file("airports.html")
+
+show(f)
